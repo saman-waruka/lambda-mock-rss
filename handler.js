6134945c1d4fa2feb9e9095fb95a1706494ea090
saman <saman_waruka@cmu.ac.th>
2020-10-05 11:33:42 +0700

    add mock rss 3

--- handler.js
+++ handler.js
@@ -23,3 +23,15 @@ module.exports.rss2 = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.rss3 = async (event) => {
+  const file = await fs.readFileSync("mock-rss-3.xml", "utf8");
+  console.log({ file });
+  return {
+    statusCode: 200,
+    body: file,
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
